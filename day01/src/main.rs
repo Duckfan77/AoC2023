ef68ac4c47ac6ff9112dc95bf554e7fc8fe5501d
Melissa <77photon@gmail.com>
2023-12-01 12:12:46 -0800

    Add Part 2 of Day01
    Added a convert_to_digit function that converts the written numbers
    into digits. This preserves the first and last letters of each word,
    to avoid issues where the text would overlap, and so converting one
    removes an earlier or later digit.
    
    This is likely not always safe, and a sufficiently evil input may be
    able to break it, but it works here.

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -30,4 +30,21 @@ fn part1(text: &str) {
     );
 }
 
-fn part2(text: &str) {}
+fn convert_to_digits(text: String) -> String {
+    // Conversions have the first and last letter preserved, to avoid issues where a
+    // replacement removes a letter that was required for an earlier number. A
+    // sufficiently evil input may be able to break this, but my input works.
+    text.replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e")
+}
+
+fn part2(text: &str) {
+    part1(&convert_to_digits(text.into()))
+}
