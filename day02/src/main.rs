ea5f75ce3324306136d001cdef50af74bc92a87e
Melissa <77photon@gmail.com>
2023-12-01 21:45:11 -0800

    Implement Day 02 Part 1
    Implemented Part 1 of Day 02, with infrastructure to hopefully make
    part 2 easier, depending on what it is.

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 fn main() {
     let text = include_str!("../input");
 
@@ -8,6 +10,76 @@ fn main() {
     part2(&text);
 }
 
-fn part1(text: &str) {}
+#[derive(Debug)]
+struct Seen {
+    red: i32,
+    green: i32,
+    blue: i32,
+}
 
-fn part2(text: &str) {}
+impl Seen {
+    fn new() -> Self {
+        Self {
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+    }
+
+    fn from_pull(pull: &str) -> Self {
+        let mut out = Self::new();
+        for dice in pull.split(", ") {
+            let (count, color) = dice.split_once(" ").expect("no space in found");
+            match color {
+                "red" => out.red = count.parse().unwrap(),
+                "green" => out.green = count.parse().unwrap(),
+                "blue" => out.blue = count.parse().unwrap(),
+                _ => panic!("Unexpected color {} in view {}", color, pull),
+            };
+        }
+        out
+    }
+
+    fn update_max(&mut self, other: &Self) {
+        self.red = max(self.red, other.red);
+        self.green = max(self.green, other.green);
+        self.blue = max(self.blue, other.blue);
+    }
+
+    fn from_game(game: &str) -> Self {
+        let mut out = Self::new();
+        for pull in game.split("; ") {
+            out.update_max(&Self::from_pull(pull));
+        }
+        out
+    }
+
+    fn possible(&self, bag: &Seen) -> bool {
+        self.red <= bag.red && self.green <= bag.green && self.blue <= bag.blue
+    }
+}
+
+const PART1_BAG: Seen = Seen {
+    red: 12,
+    green: 13,
+    blue: 14,
+};
+
+fn part1(text: &str) {
+    println!(
+        "{}",
+        text.lines()
+            .filter_map(|line| {
+                let (game_num, game) = line.split_once(": ").expect("no colon found");
+                let seen = Seen::from_game(game);
+                if seen.possible(&PART1_BAG) {
+                    Some(game_num.split_once(" ").unwrap().1.parse::<i64>().unwrap())
+                } else {
+                    None
+                }
+            })
+            .sum::<i64>()
+    );
+}
+
+fn part2(_text: &str) {}
