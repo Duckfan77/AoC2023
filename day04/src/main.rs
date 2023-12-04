fdb73dade09cefb80532867eceaffc2ea080637b
Melissa <77photon@gmail.com>
2023-12-03 22:18:09 -0800

    Implement day04 part 2
    Implemented day 04 part 2, using a vector to store the count of each card

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -40,8 +40,12 @@ impl Card {
         }
     }
 
+    pub fn overlap(&self) -> usize {
+        self.winning.intersection(&self.have).count()
+    }
+
     pub fn score(&self) -> i32 {
-        Self::overlap_to_points(self.winning.intersection(&self.have).count())
+        Self::overlap_to_points(self.overlap())
     }
 }
 
@@ -54,4 +58,19 @@ fn part1(text: &str) {
     );
 }
 
-fn part2(text: &str) {}
+fn part2(text: &str) {
+    let mut total = 0;
+    let mut cards: Vec<(i32, Card)> = text
+        .lines()
+        .map(|card| (1, Card::from_line(card)))
+        .collect();
+    for i in 0..cards.len() {
+        let current_count = cards[i].0;
+        total += current_count;
+        for add in (i + 1)..=(i + cards[i].1.overlap()) {
+            let (mod_count, _) = cards.get_mut(add).unwrap();
+            *mod_count += current_count;
+        }
+    }
+    println!("{}", total);
+}
