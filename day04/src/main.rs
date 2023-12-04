a4925db525c2b77770cb8e2ab0a85467d4ab6c86
Melissa <77photon@gmail.com>
2023-12-03 21:58:44 -0800

    Implement Day04 part 1
    Implemented day 04 part 1, using set intersection

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     let text = include_str!("../input");
 
@@ -8,6 +10,48 @@ fn main() {
     part2(&text);
 }
 
-fn part1(text: &str) {}
+struct Card {
+    winning: HashSet<i32>,
+    have: HashSet<i32>,
+}
+
+impl Card {
+    pub fn from_line(line: &str) -> Self {
+        let (win, have) = line.split_once(": ").unwrap().1.split_once(" | ").unwrap();
+        Self {
+            winning: win
+                .trim()
+                .split_whitespace()
+                .map(|num| num.parse().unwrap())
+                .collect(),
+            have: have
+                .trim()
+                .split_whitespace()
+                .map(|num| num.parse().unwrap())
+                .collect(),
+        }
+    }
+
+    fn overlap_to_points(overlap: usize) -> i32 {
+        if overlap == 0 {
+            0
+        } else {
+            2_i32.pow((overlap - 1) as u32)
+        }
+    }
+
+    pub fn score(&self) -> i32 {
+        Self::overlap_to_points(self.winning.intersection(&self.have).count())
+    }
+}
+
+fn part1(text: &str) {
+    println!(
+        "{}",
+        text.lines()
+            .map(|card| Card::from_line(card).score())
+            .sum::<i32>()
+    );
+}
 
 fn part2(text: &str) {}
