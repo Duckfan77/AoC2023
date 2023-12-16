e7ff0dfe69c7998a3e279aeea2939180f3789799
Melissa <77photon@gmail.com>
2023-12-15 21:02:05 -0800

    Day16 Setup

---
+++ day16/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let text = include_str!("../input");
+
+    println!("Part 1:");
+    part1(&text);
+
+    println!("\nPart 2:");
+    part2(&text);
+}
+
+fn part1(text: &str) {}
+
+fn part2(text: &str) {}
