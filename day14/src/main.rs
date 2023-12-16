80bbe05d729a08ca899e20521b582fb936a18bb8
Melissa <77photon@gmail.com>
2023-12-16 00:15:44 -0800

    Implement Day14 Part 2
    Implement Day 14 part 2, using a record of the past to find
    when it cycles, and how long it takes to cycle. Couldn't figure out the
    math to figure out where in the cycle it runs out, so I cheated and
    just used chained and cycling iterators.

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,4 +1,6 @@
 extern crate nalgebra as na;
+use std::collections::HashMap;
+
 use na::DMatrix;
 
 fn main() {
@@ -11,7 +13,7 @@ fn main() {
     part2(&text);
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 enum Square {
     Empty,
     Round,
@@ -41,6 +43,7 @@ impl Square {
 
 struct Platform {
     map: DMatrix<Square>,
+    past: HashMap<DMatrix<Square>, usize>,
 }
 
 impl Platform {
@@ -53,6 +56,7 @@ impl Platform {
                     .lines()
                     .flat_map(|line| line.chars().map(|c| Square::from_char(&c))),
             ),
+            past: HashMap::new(),
         }
     }
 
@@ -80,6 +84,87 @@ impl Platform {
         }
     }
 
+    fn tilt_south(&mut self) {
+        let rows = self.map.nrows();
+        for mut col in self.map.column_iter_mut() {
+            let mut open: isize = rows as isize - 1;
+            for i in (0..rows).rev() {
+                match col[i] {
+                    Square::Empty => {}
+                    Square::Round => {
+                        if i < open as usize {
+                            // need to move the rock
+                            col[open as usize] = Square::Round;
+                            col[i] = Square::Empty;
+                        }
+                        open = open - 1;
+                    }
+                    Square::Cube => {
+                        // set open to next cell
+                        open = i as isize - 1
+                    }
+                }
+            }
+        }
+    }
+
+    fn tilt_west(&mut self) {
+        let cols = self.map.ncols();
+        for mut row in self.map.row_iter_mut() {
+            let mut open = 0;
+            for i in 0..cols {
+                match row[i] {
+                    Square::Empty => {}
+                    Square::Round => {
+                        if i > open {
+                            // need to move the rock
+                            row[open] = Square::Round;
+                            row[i] = Square::Empty;
+                        }
+                        open = open + 1;
+                    }
+                    Square::Cube => {
+                        // set open to next cell
+                        open = i + 1
+                    }
+                }
+            }
+        }
+    }
+
+    fn tile_east(&mut self) {
+        let cols = self.map.ncols();
+        for mut row in self.map.row_iter_mut() {
+            let mut open: isize = cols as isize - 1;
+            for i in (0..cols).rev() {
+                match row[i] {
+                    Square::Empty => {}
+                    Square::Round => {
+                        if i < open as usize {
+                            // need to move the rock
+                            row[open as usize] = Square::Round;
+                            row[i] = Square::Empty;
+                        }
+                        open = open - 1;
+                    }
+                    Square::Cube => {
+                        // set open to next cell
+                        open = i as isize - 1
+                    }
+                }
+            }
+        }
+    }
+
+    fn cycle(&mut self) -> bool {
+        self.past.insert(self.map.clone(), self.past.len());
+        self.tilt_north();
+        self.tilt_west();
+        self.tilt_south();
+        self.tile_east();
+        self.past.contains_key(&self.map)
+    }
+
     fn calculate_load(&self) -> usize {
         let rows = self.map.nrows();
         self.map
@@ -100,6 +185,17 @@ impl Platform {
             })
             .sum::<usize>()
     }
+
+    // used for debug, don't warn for deadcode
+    #[allow(dead_code)]
+    fn print_map(&self) {
+        for row in self.map.row_iter() {
+            for square in row.iter() {
+                print!("{}", square.as_char());
+            }
+            println!();
+        }
+    }
 }
 
 fn part1(text: &str) {
@@ -110,4 +206,29 @@ fn part1(text: &str) {
     println!("{}", platform.calculate_load());
 }
 
-fn part2(text: &str) {}
+fn part2(text: &str) {
+    let mut platform = Platform::from_input(text);
+
+    for _ in 0..1_000_000_000 {
+        if platform.cycle() {
+            break;
+        }
+    }
+
+    let cycle_len = platform.past.len() - platform.past.get(&platform.map).unwrap();
+    let lead_length = platform.past.len() - cycle_len;
+
+    let n = (0..lead_length)
+        .chain(((lead_length)..(lead_length + cycle_len)).cycle())
+        .nth(1_000_000_000)
+        .unwrap();
+
+    for (map, position) in platform.past.iter() {
+        if *position == n {
+            platform.map = map.clone();
+            break;
+        }
+    }
+
+    println!("{}", platform.calculate_load());
+}
